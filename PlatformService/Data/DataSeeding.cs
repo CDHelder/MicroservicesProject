13955c3dad7e927120191da64c7600c77b7e9cc4
C Helder <christian.helder@itvitaelearning.nl>
2021-09-15 15:27:30 +0200

    Add project files.

---
+++ PlatformService/Data/DataSeeding.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public static class DataSeeding
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext dbContext)
+        {
+            if (!dbContext.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+
+                dbContext.Platforms.AddRange(
+                     new Platform() { Name="Dot Net", Publisher="Microsoft", Cost="Free"},
+                     new Platform() { Name="SQL Server", Publisher="Database", Cost="Free"},
+                     new Platform() { Name="Linux", Publisher="Apple", Cost="Free"}
+                    );
+
+                dbContext.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+}
---
+++ PlatformService/Data/PlatformRepository.cs
@@ -0,0 +1,41 @@
+﻿using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepository : IPlatformRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public PlatformRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(Platform platform)
+        {
+            if (platform == null)
+                throw new ArgumentNullException(nameof(platform));
+
+            dbContext.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAll()
+        {
+            return dbContext.Platforms.ToList();
+        }
+
+        public Platform GetById(int id)
+        {
+            return dbContext.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (dbContext.SaveChanges() > 0);
+        }
+    }
+}
